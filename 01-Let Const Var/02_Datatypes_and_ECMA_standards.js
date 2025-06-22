f76337a4e0176394d595fc5e0eac38f1dbbf6e16
Sayantan Pal <sayantanpal100@Sayantans-MacBook-Pro.local>
2025-06-23 03:30:03 +0530

    01-Let Const Var/02_Datatypes_and_ECMA_standards - Add initial data types and ECMAScript standards documentation

---
+++ 01-Let Const Var/02_Datatypes_and_ECMA_standards.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all code as new ECMAScript 5 code
+
+//alert("Hello World!"); // Uncommenting the above line will show an alert in the browser but in Node.js it will throw an error
+
+// number => consists of digits, can be positive or negative, and can include decimals
+// string => consists of characters, can include letters, numbers, and symbols
+// boolean => represents true or false values
+// null => represents an intentional absence of any value or empty value such as a variable that has been explicitly set to null
+// undefined => represents a variable that has been declared but not assigned a value such as let x;
+//symbol => represents a unique and immutable value, often used as object property keys
+
+
+
+
+// VVI
+
+console.log((typeof null)); // "object" (this is a known JavaScript quirk
+
+// console.log((typeof undefined)); // "undefined"  
+
+// console.log((typeof Symbol())); // "symbol" (ES6 feature, not available in ES5)
